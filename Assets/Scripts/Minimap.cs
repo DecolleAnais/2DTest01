d3edb97fce451975db4adfdf1b29cbcecd0e4fa6
DecolleAnais <anais_decolle@orange.fr>
2018-02-13 00:00:53 +0100

    Minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minimap : MonoBehaviour {
+
+    public Vector2 playerPosition;
+    private GameObject playerIcon;
+    private GameObject map;
+    private Vector2 topRightMap;
+    private Vector2 playerMapPosition;
+
+	// Use this for initialization
+	void Start () {
+        map = GameObject.Find("Content");
+
+        RectTransform rt = map.GetComponent<RectTransform>();
+        
+        topRightMap = new Vector2(rt.sizeDelta.x, rt.sizeDelta.y);
+
+        float x = topRightMap.x * playerPosition.x;
+        float y = topRightMap.y * playerPosition.y;
+        playerMapPosition = new Vector2(x, y);
+
+        playerIcon = GameObject.Find("PlayerIcon");
+        playerIcon.transform.localPosition = playerMapPosition;
+    }
+
+    private void Update()
+    {
+        float x = topRightMap.x * playerPosition.x;
+        float y = topRightMap.y * playerPosition.y;
+        playerMapPosition = new Vector2(x, y);
+        playerIcon.transform.localPosition = playerMapPosition;
+    }
+
+}
